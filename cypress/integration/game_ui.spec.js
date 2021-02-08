87ceb61a401a07a885decda875a57a93c0c059b5
Gabriel Romay Machado <romay.gabriel@gmail.com>
2021-02-08 18:43:55 +0900

    Add more tests, destroying player units with ui

--- cypress/integration/game_ui.spec.js
+++ cypress/integration/game_ui.spec.js
@@ -24,6 +24,54 @@ context('Game UI', () => {
     cy.get('#improve_strength').click();
     cy.get('#strength').should('contain', "Combat strength: [2].");
     cy.get('#improve_strength').click();
+
+    const stub = cy.stub()
+    cy.on('window:alert', stub);
+    cy.get('#improve_strength').click()
+    .then(() => {
+      expect(stub.getCall(0)).to.be.calledWith("You don't have enough gold!");
+    });
+
     cy.get('#strength').should('contain', "Combat strength: [2].");
+  })
+
+  it('Destroy one of the soldiers after confirmation', () => {
+    cy.get('#end_turn').click();
+
+    cy.get('#icon32a').click();
+    cy.get('#info').should('be.visible');
+
+    cy.on("window:confirm", () => true);
+    cy.get('#destroy').click();
+
+    cy.get('#icon32a').should('not.exist');
+  })
+
+  it('Do not destroy one of the soldiers after confirmation', () => {
+    cy.get('#end_turn').click();
+
+    cy.get('#icon32a').click();
+    cy.get('#info').should('be.visible');
+
+    cy.on("window:confirm", () => false);
+    cy.get('#destroy').click();
+
+    cy.get('#icon32a').should('exist');
+  })
+
+  it('Destroy all player soldiers and it\'s game over, after that games resets units and gold', () => {
+    cy.get('#icon32a').click();
+    cy.get('#info').should('be.visible');
+
+    cy.get('#improve_strength').click();
+    cy.get('#gold').should('have.value', 0);
+
+    cy.get('#destroy').click();
+
+    cy.on("window:confirm", () => true).then(() => {
+      //cy.get('#gold').should('have.value', 1); //This is a bug, currently game is not reseting gold
+      cy.get('#icon32a').should('exist');
+    });
+    cy.on("window:alert", () => true);
   })
 })
