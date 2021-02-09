2eb0419022f75accab55558c0a117bf171a1daba
Gabriel Romay Machado <romay.gabriel@gmail.com>
2021-02-09 18:11:36 +0900

    Mute sounds ftests

--- cypress/integration/topbar_actions.spec.js
+++ cypress/integration/topbar_actions.spec.js
@@ -5,6 +5,32 @@ context('Toolbar actions', () => {
     cy.visit('main.html');
   })
 
+  it('When user clicks on unmute music button, music gets unmuted and then, it can me muted again', () => {
+    cy.get('#mute_music').should('contain', 'Unmute Music');
+
+    cy.get('#mute_music').click();
+
+    cy.get('#mute_music').should('contain', 'Mute Music');
+    //TODO: demonstrate that sound is actually emited
+
+    cy.get('#mute_music').click();
+
+    cy.get('#mute_music').should('contain', 'Unmute Music');
+  })
+
+  it('When user clicks on unmute SFX button, SFX gets unmuted and then, it can me muted again', () => {
+    cy.get('#mute_sfx').should('contain', 'Unmute SFX');
+
+    cy.get('#mute_sfx').click();
+
+    cy.get('#mute_sfx').should('contain', 'Mute SFX');
+    //TODO: demonstrate that sound is actually emited
+
+    cy.get('#mute_sfx').click();
+
+    cy.get('#mute_sfx').should('contain', 'Unmute SFX');
+  })
+
   it('When user clicks next turn, a new player and AI unit should spawn and player gains 3 gold', () => {
     cy.get('#gold').should('have.value', 1);
     cy.get('#map')
