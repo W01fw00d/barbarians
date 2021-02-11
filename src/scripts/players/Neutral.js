14fd3e9746c74dd081bd216d30d7a22c88dc82ae
Gabriel Romay Machado <romay.gabriel@gmail.com>
2021-02-11 13:14:24 +0900

    Add TODO

--- src/scripts/players/Neutral.js
+++ src/scripts/players/Neutral.js
@@ -2,5 +2,5 @@ function Neutral() {
     Player.call(this);
     this.name = 'neutral';
 }
-
+//TODO: create yet another parent class to share between human, ai and neutral; that doesn't include move functions and other things only needed by ai and human
 Neutral.prototype = Object.create(Player.prototype);
