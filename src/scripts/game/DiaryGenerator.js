2d8a20fac2839200c895afa7d79a781ebd58da37
Gabriel Romay Machado <romay.gabriel@gmail.com>
2021-02-14 17:22:32 +0900

    Improve narration with all killer-victim options

--- src/scripts/game/DiaryGenerator.js
+++ src/scripts/game/DiaryGenerator.js
@@ -9,7 +9,15 @@ DiaryGenerator.prototype.dead = function(killer, victim) {
     neutral: 'a wolf',
   };
 
-  const humanText = () => {
+  const humanAi = () => {
+    return `A barbarian was defeated that day by ${killer.name}.`;
+  };
+
+  const humanNeutral = () => {
+    return `A wolf pack attacked ${killer.name}. Luckily, our soldier slayed the beast.`
+  };
+
+  const aiHuman = () => {
     const getPostPhrase = () => {
       const postPhrases = [
         '',
@@ -27,28 +35,45 @@ DiaryGenerator.prototype.dead = function(killer, victim) {
       return postPhrases[getRandomInt(0, postPhrases.length)];
     }
 
-    return `${victim.name}, the ${playerMobMap[victim.player]}, ` +
-      `was defeated that day by ${killer.name}, the ${playerMobMap[killer.player]}.${getPostPhrase()}`;
+    return `${victim.name}, one of our own, was killed today by a damn barbarian.${getPostPhrase()}`;
   };
 
-  const aiText = () => {
-    return `${victim.name}, the ${playerMobMap[victim.player]}, ` +
-      `was defeated that day by ${killer.name}, the ${playerMobMap[killer.player]}.`;
+  const aiNeutral = () => {
+    return `It seems that the barbarians are hungry, there have been reports of them hunting some wolves.`;
   };
 
-  const neutralText = () => {
-    return `A wolf pack attacked, ` +
-      `${killer.name}, the ${playerMobMap[killer.player]}. Luckily, the human slayed the beast.`
+  const neutralHuman = () => {
+    return `A misfortune happened today. A wolf pack attacked ${victim.name} out of the camp: we found the soldier already dead.`
   };
 
-  // Increase depth of map: each mob with the other 2. Examples:
-  // human victim, wolves killer
-  // ai victim, wolves killer
-  const playerTextMap = {
-    human: humanText,
-    ai: aiText,
-    neutral: neutralText,
+  const neutralAi = () => {
+    const getPostPhrase = () => {
+      const postPhrases = [
+        '',
+        ' Even nature hate them.',
+        ' Beasts killing beasts.',
+      ];
+
+      return postPhrases[getRandomInt(0, postPhrases.length)];
+    }
+
+    return `It seems that some wolves ate a barbarian alive today.${getPostPhrase()}`;
   };
 
-  return playerTextMap[victim.player]();
+  const killerVictimMap = {
+    human: {
+      ai: humanAi,
+      neutral: humanNeutral,
+    },
+    ai: {
+      human: aiHuman,
+      neutral: aiNeutral,
+    },
+    neutral: {
+      human: neutralHuman,
+      ai: neutralAi,
+    },
+  };
+
+  return killerVictimMap[killer.player][victim.player]();
 }
